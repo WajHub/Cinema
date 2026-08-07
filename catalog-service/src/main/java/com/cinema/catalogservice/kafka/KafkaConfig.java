c6b65174fb8b1b8c70859944cf44d2f0eac85a62
WajHub <hubert.wajda03@gmail.com>
2026-08-07 15:38:55 +0200

    refactor http request

--- catalog-service/src/main/java/com/cinema/catalogservice/kafka/KafkaConfig.java
+++ catalog-service/src/main/java/com/cinema/catalogservice/kafka/KafkaConfig.java
@@ -1,5 +1,11 @@
 package com.cinema.catalogservice.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
 @Configuration
 public class KafkaConfig {
   @Bean
