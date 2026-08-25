9666f1e7f8730f7cdaca5b9e712f5227775bb04a
WajHub <hubert.wajda03@gmail.com>
2026-08-25 22:39:38 +0200

    feat: remove unused relation between booking and movie session

--- booking-service/src/main/java/com/cinema/bookingservice/entity/BookingEntity.java
+++ booking-service/src/main/java/com/cinema/bookingservice/entity/BookingEntity.java
@@ -1,7 +1,5 @@
 package com.cinema.bookingservice.entity;
 
-import java.math.BigDecimal;
-import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,6 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,10 +29,6 @@ public class BookingEntity {
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
   @JoinColumn(name = "user_id", nullable = false)
   private UserEntity user;
-
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "session_id", nullable = false)
-  private MovieSessionEntity session;
 
   @Column(name = "total_price", nullable = false, precision = 12, scale = 2)
   private BigDecimal totalPrice;
